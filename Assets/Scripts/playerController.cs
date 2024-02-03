c649b7eb350269a5ec6a8b6696b20e1e6efba7c3
Dogancan Oz <doganlouysa@outlook.com>
2024-02-03 14:12:00 +0300

    powerup prefabs and its script added

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class playerController : MonoBehaviour
 {
     [SerializeField] private TMP_Text coinText;
     private int count = 0;
+    public ThirdPersonController ThirdPersonController;
+    [SerializeField] private Image powerupImage;
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,5 +21,19 @@ public class playerController : MonoBehaviour
             coinText.text = count.ToString();
             other.gameObject.SetActive(false); 
         }
+
+        if (other.gameObject.CompareTag("powerup"))
+        {
+            ThirdPersonController.playerSpeed = 10f;
+            other.gameObject.SetActive(false);
+            powerupImage.gameObject.SetActive(true);
+            Invoke("resetPlayerSpeed",5.0f);
+        }
+    }
+
+    public void resetPlayerSpeed()
+    {
+        ThirdPersonController.playerSpeed = 5f;
+        powerupImage.gameObject.SetActive(false);
     }
 }
